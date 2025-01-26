204aada44982f2dc42c4183b5a4150a6f792ba9b
Spectacular2001 <ruchitadhore2001@gmail.com>
2025-01-26 22:28:18 +0530

    Implemented polymorphism with overloaded methods
    Implemented polymorphism with overloaded methods

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
-package com.driver;
+package com.driver; // Ensure this matches the directory structure
 
 public class Main {
+    static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        System.out.println("Product of 2 integers: " + p.product(5, 4));
+        System.out.println("Product of 3 integers: " + p.product(5, 4, 2));
+        System.out.println("Product of 2 doubles: " + p.product(5.5, 4.4));
+    }
 }
